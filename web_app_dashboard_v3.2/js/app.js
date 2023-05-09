f19f3b22266d293ffe558c1a5e17c23cecffd6a8
lexborelli <alexasborelli@gmail.com>
2023-05-09 12:35:14 -0400

    added javascript

--- web_app_dashboard_v3.2/js/app.js
+++ web_app_dashboard_v3.2/js/app.js
@@ -42,4 +42,94 @@ let trafficOptions = {
     }
 };
 
+let trafficChart = new Chart(trafficCanvas, {
+    type: 'line', 
+    data: trafficData, 
+    options: trafficOptions
+});
+
+const dailyCanvas = document.getElementById("daily-chart");
+
+    const dailyData = {
+        labels: ["S", "M", "T", "W", "T", "F", "S"], 
+        datasets: [{
+            label: '# of Hits',
+            data:[75, 115, 175, 125, 225, 200, 100],
+            backgroundColor: '#7477BF',
+            borderWidth: 1
+        }]
+    };
+
+    const dailyOptions = {
+        scales: {
+            y: {
+                beginAtZero: true 
+            }
+        },
+        plugins: {
+            legend: {
+                display: false
+            }
+        }
+    };
+
+    let dailyChart = new Chart(dailyCanvas, {
+        type: 'bar', 
+        data: dailyData, 
+        options: dailyOptions
+    });
+
+    const mobileCanvas = document.getElementById("mobile-chart");
+
+    const mobileData = {
+        labels: ["Desktop", "Tablet", "Phones"], 
+        datasets: [{
+            labels:'# of Users', 
+            data: [2000, 550, 500],
+            borderWidth: 0, 
+            backgroundColor: [
+                '#7477BF', 
+                '#78CF82',
+                '#51B6C8'
+            ]
+        }]
+    };
+
+    const mobileOptions = {
+        aspectRatio: 1.9, 
+        plugins: {
+            legend: {
+                position: 'right', 
+                labels: {
+                    boxWidth: 20, 
+                    fontStyle: 'bold'
+                }
+            }
+        }
+    }; 
+
+    let mobileChart = new Chart(mobileCanvas, {
+        type: 'doughnut', 
+        data: mobileData, 
+        options: mobileOptions
+    });
+
+    const user = document.getElementById("userFields"); 
+    const message = document.getElementById("messageField");
+    const send = document.getElementById("send"); 
+
+    send.addEventListener('click', () => {
+        if (user.value === "" && message.value ==="") {
+            alert("Please fill out user and message fields before sending"); 
+        } else if(user.value === "") {
+            alert("Please fill out user field before sending");
+        } else if(message.value ==="") {
+            alert("Please fill out message field before sending");
+        } else {
+            alert(`Message successfully sent to: ${user.value}`);
+        }
+    });
+
+
+
 
