fe26df2c7acbe4e950cc60d68ca61f06fad4bfdd
lexborelli <alexasborelli@gmail.com>
2023-06-02 21:58:44 -0400

    js adjustments with send button

--- web_app_dashboard_v3.2/js/app.js
+++ web_app_dashboard_v3.2/js/app.js
@@ -135,16 +135,17 @@ const dailyCanvas = document.getElementById("daily-chart");
         options: mobileOptions
     });
 
-    const user = document.getElementById("userFields"); 
+    const user = document.getElementById("userField"); 
     const message = document.getElementById("messageField");
     const send = document.getElementById("send"); 
 
     send.addEventListener('click', () => {
-        if (user.value === "" && message.value ==="") {
+        
+        if (user.value === "" && message.value === "") {
             alert("Please fill out user and message fields before sending"); 
         } else if(user.value === "") {
             alert("Please fill out user field before sending");
-        } else if(message.value ==="") {
+        } else if(message.value === "") {
             alert("Please fill out message field before sending");
         } else {
             alert(`Message successfully sent to: ${user.value}`);
